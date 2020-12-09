1080262b84545436823e90ee44c787754c58f8ee
Maurício de Bastiani Silva <mauriciobastiani@outlook.com>
2020-12-09 09:05:49 -0300

    Solution created

---
+++ src/Rss.Feed.Reader.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rss.Feed.Reader.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Rss.Feed.Reader.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Rss.Feed.Reader.Core
+{
+    public class Class1
+    {
+    }
+}
